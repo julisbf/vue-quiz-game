292ef4fc35bfc8c5218a34401c639d63febf0cef
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-08-03 11:08:57 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1 class="text-center">The Super Quiz</h1>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <app-wrong v-show="isWrong" @wrong="isWrong = !isWrong"></app-wrong>
+        <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'" v-show="!isWrong"></component>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Question from './components/Question.vue';
+    import Answer from './components/Answer.vue';
+    import Wrong from './components/Wrong.vue';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data() {
+            return {
+              mode: 'app-question',
+              isWrong: false
+            }
+        },
+        methods: {
+          answered(isCorrect) {
+              if (isCorrect) {
+                this.mode = 'app-answer';
+              } else {
+                this.isWrong = !this.isWrong;
+                //this.mode = 'app-question';
+                
+                  //alert('Wrong, try again!');
+              }
+          }
+        },
+        components: {
+            appQuestion: Question,
+            appAnswer: Answer,
+            appWrong: Wrong
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Wrong.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="alert alert-danger text-center">
+    <h1>Oops! That's inorrect!</h1>
+    <hr>
+    <button class="btn btn-primary" @click="tryAgain">Try Again!</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    tryAgain() {
+      this.$emit('wrong');
+    }
+  }
+}
+</script>
