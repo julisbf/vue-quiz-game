b520ff3e3df63cf0229bcb510659a9c85259949b
Juliana BF <33138019+julisbf@users.noreply.github.com>
2018-08-07 23:28:29 -0400

    Add styles to the alert

--- src/components/Wrong.vue
+++ src/components/Wrong.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="alert alert-danger text-center">
+  <div class="alert alert-danger text-center wrong">
     <h1>Oops! That's inorrect!</h1>
     <hr>
     <button class="btn btn-primary" @click="tryAgain">Try Again!</button>
@@ -14,3 +14,11 @@ export default {
   }
 }
 </script>
+<style scoped>
+.wrong {
+  position: absolute;
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+}
+</style>
